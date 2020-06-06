87f83a792f77c5313fc036050535ca6bd2649a42
Bruno Girome <brunogirome@gmail.com>
2020-06-06 19:24:40 -0300

    Added default property to Title component fontSize

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const Title = ({ children, fontSize }) => {
+const Title = ({ children, fontSize = '24px' }) => {
   return <Container fontSize={fontSize}>{children}</Container>;
 };
 
